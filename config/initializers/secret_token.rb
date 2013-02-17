d7d0c7df669cbacc4f3fe671c7726e9fc0676307
Sarah Edris <sredris@gmail.com>
2013-02-17 15:01:09 -0500

    Made Site resource and removed index.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SedrisProj1::Application.config.secret_token = 'da7dd9311fc1b72fac6a386eefd66bbb60443a0452d8b90ec26c88656c7ab171ea982b2df3b87661f231d650bc11558c1d48ed43f5e4cd8407851770eedf9fed'
